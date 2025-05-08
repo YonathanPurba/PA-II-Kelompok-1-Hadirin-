33ce8e0bdc393714586cf583ec27297e9fce2fed
Yonathan Purba <yonathanpurba@yahoo.com>
2025-05-08 11:51:14 +0700

    hapus tambah pengguna

--- resources/views/admin/pages/users/manajemen_data_users.blade.php
+++ resources/views/admin/pages/users/manajemen_data_users.blade.php
@@ -119,12 +119,6 @@
                     </div>
                 @endif
                 
-                <!-- Tombol Tambah -->
-                <div class="mt-4 text-end">
-                    <a href="{{ route('users.create') }}" class="btn btn-success">
-                        <i class="bi bi-plus-circle me-1"></i> Tambah Pengguna
-                    </a>
-                </div>
             </div>
         </div>
     </main>
